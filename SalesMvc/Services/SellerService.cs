0214393b8a84b4f1a34cc67f246f2c8307740641
Thiago Tadeu <thiago_tvieira@hotmail.com>
2019-09-13 17:04:41 -0300

    Delete Seller

--- SalesMvc/Services/SellerService.cs
+++ SalesMvc/Services/SellerService.cs
@@ -20,11 +20,25 @@ namespace SalesMvc.Services
             return _context.Seller.ToList();
         }
 
+
         public void Insert (Seller seller)
         {
             _context.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove ( int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
 
     }
 }

54d59ee3a5261bd45b70dc767abe45e13d35b5b6
Thiago Tadeu <thiago_tvieira@hotmail.com>
2019-09-18 15:46:09 -0300

    Exception Delete FK

--- SalesMvc/Services/SellerService.cs
+++ SalesMvc/Services/SellerService.cs
@@ -36,15 +36,22 @@ namespace SalesMvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException )
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
         {
             bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
-            if (! hasAny )
+            if (!hasAny)
             {
                 throw new NotFoundException("Id not found");
             }
